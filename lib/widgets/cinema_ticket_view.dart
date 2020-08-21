ba3f44abe32f08d7b44ffe4f7548fb8840fe40a9
Tonia Tkachuk <tonia.tkachuk@gmail.com>
2020-08-21 09:57:21 +0300

    Removed pre-defined height for a parent container.

--- lib/widgets/cinema_ticket_view.dart
+++ lib/widgets/cinema_ticket_view.dart
@@ -19,139 +19,131 @@ class CinemaTicket extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: 220.0,
-          child: LayoutBuilder(builder: (context, constraints) {
-            return IntrinsicHeight(
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      flex: 5,
-                      child: ClipPath(
-                        clipper: CircleClipper(radius: circleRadius),
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              vertical: padding, horizontal: padding),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(borderRadius),
-                            gradient: LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              colors: [kColorBeigeStart, kColorBeigeEnd],
-                            ),
-                          ),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                ticketData.number,
-                                style: kNumberSmallTextStyle,
-                              ),
-                              Text(
-                                'TICKET',
-                                style: GoogleFonts.amiri(
-                                  textStyle: kTicketTextStyle,
-                                ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Text(
-                                    '★',
-                                    style: kCinemaStarTextStyle,
-                                  ),
-                                  Text(
-                                    'CINEMA',
-                                    style: kCinemaTextStyle,
-                                  ),
-                                  Text(
-                                    '★',
-                                    style: kCinemaStarTextStyle,
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                'Row ${ticketData.row} Seat ${ticketData.seat}',
-                                style: kPlaceTextStyle,
-                              ),
-                              Text(
-                                'Date ${ticketData.formattedDate} Time ${ticketData.formattedTime}',
-                                style: kDateTextStyle,
-                              ),
-                            ],
-                          ),
-                        ),
+        body: SafeArea(
+      child: IntrinsicHeight(
+        child: Row(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Expanded(
+            flex: 5,
+            child: ClipPath(
+              clipper: CircleClipper(radius: circleRadius),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                    vertical: padding, horizontal: padding),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(borderRadius),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [kColorBeigeStart, kColorBeigeEnd],
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      ticketData.number,
+                      style: kNumberSmallTextStyle,
+                    ),
+                    Text(
+                      'TICKET',
+                      style: GoogleFonts.amiri(
+                        textStyle: kTicketTextStyle,
                       ),
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(borderRadius),
-                        color: Colors.white,
-                      ),
-                      child: Center(
-                        child: DottedLine(
-                          direction: Axis.vertical,
-                          lineLength: constraints.maxHeight,
-                          lineThickness: 2.0,
-                          dashLength: 4.0,
-                          dashColor: Colors.transparent,
-                          dashRadius: 0.0,
-                          dashGapLength: 4.0,
-                          dashGapColor: kColorPinkStart,
-                          dashGapRadius: 0.0,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          '★',
+                          style: kCinemaStarTextStyle,
                         ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 3,
-                      child: ClipPath(
-                        clipper: CircleClipper(
-                            radius: circleRadius, side: CircleSide.right),
-                        child: RotatedBox(
-                          quarterTurns: 3,
-                          child: Container(
-                            padding: EdgeInsets.all(padding),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(borderRadius),
-                              gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [kColorPinkStart, kColorPinkEnd],
-                              ),
-                            ),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text(
-                                  ticketData.number,
-                                  style: kNumberBigTextStyle,
-                                ),
-                                BarCodeImage(
-                                  padding: EdgeInsets.only(top: 8.0),
-                                  params: Code93BarCodeParams(
-                                    ticketData.number,
-                                    lineWidth: 1.5,
-                                    barHeight: 56.0,
-                                    withText: false,
-                                  ),
-                                  onError: (error) {
-                                    print('barcode error: $error');
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
+                        Text(
+                          'CINEMA',
+                          style: kCinemaTextStyle,
                         ),
-                      ),
+                        Text(
+                          '★',
+                          style: kCinemaStarTextStyle,
+                        ),
+                      ],
                     ),
-                  ]),
-            );
-          }),
-        ),
+                    Text(
+                      'Row ${ticketData.row} Seat ${ticketData.seat}',
+                      style: kPlaceTextStyle,
+                    ),
+                    Text(
+                      'Date ${ticketData.formattedDate} Time ${ticketData.formattedTime}',
+                      style: kDateTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              color: Colors.white,
+            ),
+            child: Center(
+              child: DottedLine(
+                direction: Axis.vertical,
+                lineLength: 220.0,
+                lineThickness: 2.0,
+                dashLength: 4.0,
+                dashColor: Colors.transparent,
+                dashRadius: 0.0,
+                dashGapLength: 4.0,
+                dashGapColor: kColorPinkStart,
+                dashGapRadius: 0.0,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: ClipPath(
+              clipper:
+                  CircleClipper(radius: circleRadius, side: CircleSide.right),
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Container(
+                  padding: EdgeInsets.all(padding),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(borderRadius),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [kColorPinkStart, kColorPinkEnd],
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        ticketData.number,
+                        style: kNumberBigTextStyle,
+                      ),
+                      BarCodeImage(
+                        padding: EdgeInsets.only(top: 8.0),
+                        params: Code93BarCodeParams(
+                          ticketData.number,
+                          lineWidth: 1.5,
+                          barHeight: 56.0,
+                          withText: false,
+                        ),
+                        onError: (error) {
+                          print('barcode error: $error');
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
-    );
+    ));
   }
 }
